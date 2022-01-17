dcf1589d82b6eab48c8092b798c9db22035af742
Nishan Roka <c0837286@mylambton.ca>
2022-01-17 18:56:58 -0500

    Initial Commit

---
+++ Lab1-NishanRoka-c0837286/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab1-NishanRoka-c0837286
+//
+//  Created by Nishu Roka on 17/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
